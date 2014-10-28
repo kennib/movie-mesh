ba8c27855e5e227e2030de1f3d3ddfbe011a77a4
Kenni <kenni.bawden@gmail.com>
2014-10-28 12:32:48 +1100

    Added peer-to-peer video

--- index.js
+++ index.js
@@ -1,19 +1,68 @@
 // Import RTC libraries and plugins
 var quickconnect = require('rtc-quickconnect');
 var mesh = require('rtc-mesh');
+var media = require('rtc-media');
 
 
 // Initialise the connection
-var qc = quickconnect('http://rtc.io/switchboard', {
-  room: 'movie-mesh',
+var qc = quickconnect('http://switchboard.rtc.io', {
+  room: 'movie-mesh-crdt',
 });
 
-// Create the model
-var model = mesh(qc);
+// Capture local media
+var localMedia = media();
 
-// Report data change events
-model.on('change', function(key, value) {
-  console.log('captured change key: "' + key + '" set to ', value);
+// Give and recieve video from peers
+localMedia.once('capture', function(stream) {
+  // Share video
+  qc.addStream(stream);
+  
+  // Recieve video
+  qc.on('call:started', function(id, pc, data) {
+    console.log('peer connected: ', id);
+
+    // Render the remote streams
+    pc.getRemoteStreams().forEach(addStream(id));
+  })
+  // when a peer leaves, remove the media
+  .on('call:ended', function(id) {
+    // Remove media for the target peer from the dom
+    (peerMedia[id] || []).splice(0).forEach(function(el) {
+      el.parentNode.removeChild(el);
+    });
+  })
 });
 
-model.set('lastjoin', Date.now());
+
+// Create the elements
+var streams = document.createElement('div');
+streams.className = 'streams';
+document.body.appendChild(streams);
+
+// Render a video
+function renderMedia(media) {
+    var stream = document.createElement('div');
+    stream.className = 'stream';
+    streams.appendChild(stream);
+    
+    media.render(stream);
+
+    return stream;
+}
+
+
+// Add and render a remote video
+var peerMedia = {};
+function addStream(id) {
+  // Create the peer media list
+  peerMedia[id] = peerMedia[id] || [];
+
+  return function(stream) {
+    var remoteMedia = media(stream);
+    peerMedia[id] = peerMedia[id].concat(renderMedia(remoteMedia));
+  }
+}
+
+// Render the local media
+renderMedia(localMedia);
+
